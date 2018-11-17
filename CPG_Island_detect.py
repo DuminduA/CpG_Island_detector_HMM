c5b48fee41a0a10954d6ff12a001655470411b35
Akalanka <duminduakalanka@gmail.com>
2018-11-17 06:36:37 +0530

    created functions building_transition_matrix main

--- CPG_Island_detect.py
+++ CPG_Island_detect.py
@@ -4,3 +4,18 @@ def readfile(filename):
         gene_seqeunce = filename.read().replace('\n', '')
     return gene_seqeunce
 
+def main(filename):
+    gene_seqence = readfile(filename)
+
+def building_transition_matrix():
+    filename = "probabilities.txt"
+    with open(filename, "r") as file:
+        gene_seqeunce = file.read().replace('\n', '').split("#")
+    print(gene_seqeunce)
+    for i in range(0, len(gene_seqeunce)):
+        if(gene_seqeunce[i].__contains__("Transition matrix")):
+            transition_matrix_temp = gene_seqeunce[i+1].split("break")
+            print(transition_matrix_temp)
+
+
+building_transition_matrix()
